43ac2b7124e4642f7a832a3163050aeb51cab228
Mohamed EL Habib <mohamed.el-habib@digitaslbi.fr>
2019-12-05 10:21:40 +0100

    init project

---
+++ src/main/java/com/example/demoistio/DemoIstioApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoistio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoIstioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoIstioApplication.class, args);
+	}
+
+}
