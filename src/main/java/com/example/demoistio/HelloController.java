d2bed3c3823333a95dde52a0e340dd17a6591a91
Mohamed EL Habib <mohamed.el-habib@digitaslbi.fr>
2019-12-09 05:20:14 +0100

    Version 2 : create a new version of the api

--- src/main/java/com/example/demoistio/HelloController.java
+++ src/main/java/com/example/demoistio/HelloController.java
@@ -1,20 +1,37 @@
 package com.example.demoistio;
 
 import com.github.javafaker.Faker;
+import com.github.javafaker.Name;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.Data;
+
 @RestController
-@RequestMapping("/v1")
+@RequestMapping("/v2")
 public class HelloController {
 
-  private static final String template = "Hello, %s!";
-  private static final Faker faker = new Faker();
+    private static final Faker faker = new Faker();
 
-  
-  @RequestMapping("/hello")
-  public String hello() {
-    return String.format(template, faker.name().fullName());
+    @RequestMapping("/hello")
+    public Person hello() {
+    return new Person(faker.name());
   }
+
+  @Data  
+  class Person {
+
+        private String firstName;
+        private String middleName;
+        private String lastName;
+
+        public Person(Name name) {
+            this.firstName = name.firstName();
+            this.middleName = name.nameWithMiddle();
+            this.lastName = name.lastName();
+        }
+      
+  }
+
 }
