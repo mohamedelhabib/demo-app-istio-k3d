59a0fb475f9bdc116ff1261ee064a6b9fe05ad8e
Mohamed EL Habib <mohamed.el-habib@digitaslbi.fr>
2020-09-06 15:09:43 +0200

    version 1.2.1 : added birthday and canary release routing

--- src/main/java/com/example/demoistio/HelloController.java
+++ src/main/java/com/example/demoistio/HelloController.java
@@ -2,36 +2,35 @@ package com.example.demoistio;
 
 import com.github.javafaker.Faker;
 import com.github.javafaker.Name;
-
+import java.util.Date;
+import lombok.Data;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import lombok.Data;
 
 @RestController
 @RequestMapping("/v2")
 public class HelloController {
 
-    private static final Faker faker = new Faker();
+  private static final Faker faker = new Faker();
 
-    @RequestMapping("/hello")
-    public Person hello() {
+  @RequestMapping("/hello")
+  public Person hello() {
     return new Person(faker.name());
   }
 
-  @Data  
+  @Data
   class Person {
 
-        private String firstName;
-        private String middleName;
-        private String lastName;
+    private String firstName;
+    private String middleName;
+    private String lastName;
+    private Date birthDay;
 
-        public Person(Name name) {
-            this.firstName = name.firstName();
-            this.middleName = name.nameWithMiddle();
-            this.lastName = name.lastName();
-        }
-      
+    public Person(Name name) {
+      this.firstName = name.firstName();
+      this.middleName = name.nameWithMiddle();
+      this.lastName = name.lastName();
+      this.birthDay = faker.date().birthday();
+    }
   }
-
 }
